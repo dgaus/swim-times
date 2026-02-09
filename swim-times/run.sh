d6447c3586c9aef5fb41c7382af49a6aa9821f06
Diego Gaustein <diego.gaustein@intruder.io>
2026-02-09 14:30:05 -0300

    Improve Dockerfile

--- swim-times/run.sh
+++ swim-times/run.sh
@@ -2,13 +2,14 @@
 set -e
 
 # Ensure data directory exists
-# If config folder is mapped (HA way), use it
 if [ -d "/data" ]; then
     echo "Using Home Assistant /data directory for persistence"
     export DATA_DIR="/data"
 else
     # Ensure local data directory exists if not using HA
-    mkdir -p /app/data
+    echo "Using Home Assistant /data directory for persistence"
+    mkdir -p data
 fi
 
-npm start
+# Use exec to let node handle signals and be the primary process
+exec npm start --prefix /app
